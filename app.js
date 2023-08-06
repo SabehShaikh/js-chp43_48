7cf9c0b651fa4d9db46c3b6a6c756bda93c0e17b
SabehShaikh <121764329+SabehShaikh@users.noreply.github.com>
2023-08-06 23:40:17 +0500

    only 1 tasks done, 4 left.

---
+++ app.js
@@ -0,0 +1,13 @@
+// Assignment # 38-42 EVENTS
+
+// document.write("<h1> EVENTS </h1>");
+
+// document.write("<h2> Chapter 43-48 </h2>");
+
+/* QUESTION 01
+1. 1. Show an alert box on click on a link.  */
+// document.write("<h2> Question 01 </h2>")
+
+function showAlert() {
+    alert('Hello there, you are going to Github');
+}
